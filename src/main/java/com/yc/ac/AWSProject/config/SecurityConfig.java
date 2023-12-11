9ea3ee35bebd4d7d02fc58590a3e1762f1ac899a
jdy2225 <rina4674@naver.com>
2023-12-12 06:06:59 +0900

    시큐리티

--- src/main/java/com/yc/ac/AWSProject/config/SecurityConfig.java
+++ src/main/java/com/yc/ac/AWSProject/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.yc.ac.AWSProject.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -16,6 +17,13 @@ import java.util.Arrays;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password(passwordEncoder().encode("1234")).roles("USER")
+                .and()
+                .withUser("admin").password(passwordEncoder().encode("1234")).roles("ADMIN");
+    }
 
 
     private Object corsConfigurationSource() {
