3da888b2ff13bf2cfa0e0e8c134742d373e79c46
jdy2225 <rina4674@naver.com>
2023-12-12 06:37:21 +0900

    시큐리티  제거

--- src/main/java/com/yc/ac/AWSProject/Controller/UserController.java
+++ src/main/java/com/yc/ac/AWSProject/Controller/UserController.java
@@ -10,44 +10,28 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping(value = "/AWSProject", produces = "application/json; charset=UTF-8")
+@RequestMapping(produces = "application/json; charset=UTF-8")
 public class UserController {
     @Autowired
     UserService userService;
 
     @PostMapping("/login")
-    public ResponseEntity<User> login(@RequestBody User user) {
+    public void login(@RequestBody User user) {
         User loginUserData = user;
-        if (loginUserData != null) {
-            return ResponseEntity.ok(loginUserData);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
     }
 
     @PostMapping("/insert")
-    public ResponseEntity insert(@RequestBody User user)  {
-        if(userService.insert(user)){
-            return ResponseEntity.status(HttpStatus.CREATED).build();
-        }else{
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-        }
+    public void insert(@RequestBody User user)  {
+        userService.insert(user);
     }
     @PostMapping("/update")
-    public ResponseEntity update(@RequestBody User user)  {
-        if(userService.update(user)){
-            return ResponseEntity.status(HttpStatus.OK).build();
-        }else{
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-        }
+    public void update(@RequestBody User user)  {
+        userService.update(user);
+
     }
     @PostMapping("/delete")
-    public ResponseEntity delete(@RequestParam String id){
-        if(userService.delete(id)){
-            return ResponseEntity.status(HttpStatus.OK).build();
-        }else{
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-        }
+    public void delete(@RequestParam String id){
+        userService.delete(id);
     }
 
 }
